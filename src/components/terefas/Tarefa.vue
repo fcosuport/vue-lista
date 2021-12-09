08193cc087a346c6c78f2487b994611c55965dcd
Francisco <fcosuport@gmail.com>
2021-12-08 20:34:08 -0400

    terefa

---
+++ src/components/terefas/Tarefa.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <v-list-item
+    :class="{'green lighten-4':tarefa.concluido}"
+    @click="tarefa.concluido = !tarefa.concluido"
+    >
+      <template v-slot:default="{}">
+        <v-list-item-action>
+          <v-checkbox :input-value="tarefa.concluido"></v-checkbox>
+        </v-list-item-action>
+
+        <v-list-item-content>
+          <v-list-item-title
+          :class="{'text-decoration-line-through' :tarefa.concluido}"
+          class="text-h6"
+          >{{tarefa.titulo}}</v-list-item-title>
+        </v-list-item-content>
+      </template>
+    </v-list-item> 
+  </div>
+  
+</template>
+
+<script>
+export default {
+  props: ['tarefa']
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/Tarefas.vue
@@ -0,0 +1,64 @@
+<template>
+  <div>
+    <v-col
+      cols="12"
+    >
+      <v-text-field
+        v-model="campoInput"
+        label="Digite uma Tarefa"
+        outlined
+        clearable
+        @keyup.enter="funcAddTarefa"
+      ></v-text-field>
+    </v-col>
+    <v-list
+      flat
+      subheader
+    >
+      <v-list-item-group
+        multiple
+        active-class=""
+      >
+      <div
+      v-for="tarefa, index in tarefas" :key="index"
+      >
+        <Tarefa :tarefa="tarefa"/>
+      </div>
+      </v-list-item-group>
+    </v-list>
+  </div>
+  
+</template>
+
+<script>
+  import Tarefa from '../components/terefas/Tarefa.vue'
+
+  export default {
+    name: 'Home',
+
+    components: {
+      Tarefa
+    },
+    data() {
+      return{
+        campoInput: null,
+        tarefas: [
+          { titulo:"ir ao mercado", concluido:false },
+          { titulo:"ir ao mercado", concluido:false },
+          { titulo:"ir ao carrefour", concluido:false }
+        ]
+      }
+    },
+    methods:{
+      funcAddTarefa(){
+        if(this.campoInput){
+          this.tarefas.push({
+            titulo:this.campoInput,
+            concluido:false
+          })
+          this.campoInput = null
+        }
+      }
+    }
+  }
+</script>
