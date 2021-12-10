20fd8cacf861b3a0963e59aea720326dda562754
Francisco <fcosuport@gmail.com>
2021-12-10 11:58:08 -0400

    update

---
+++ dist/precache-manifest.0b458ddd7cf0cbc731a2d315ad32eba7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c52ebbc5d33eb89af36",
+    "url": "/css/chunk-vendors.2eb572a8.css"
+  },
+  {
+    "revision": "eb9425ab8c6b0b65474de93378239250",
+    "url": "/img/bg.eb9425ab.jpg"
+  },
+  {
+    "revision": "56bed02d6c98a8e58e84df30327a07be",
+    "url": "/index.html"
+  },
+  {
+    "revision": "473d8ea64d9f00b9402c",
+    "url": "/js/app.e7032543.js"
+  },
+  {
+    "revision": "dee562c5afde14391a0b",
+    "url": "/js/chunk-2d22d746.ae3115a2.js"
+  },
+  {
+    "revision": "2c52ebbc5d33eb89af36",
+    "url": "/js/chunk-vendors.958f1ef4.js"
+  },
+  {
+    "revision": "eb7d38325433b7428cf3cd9ae7236e3e",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
