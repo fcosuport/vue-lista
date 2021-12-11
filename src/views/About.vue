8f6cd57e81eea1334728170a6462f19a21e9fe44
Francisco <fcosuport@gmail.com>
2021-12-11 11:35:24 -0400

    page sobre

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,10 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div 
+  class="about mt-5 ml-5"
+  >
+    <h1>Desenvolvido por Prático Sistemas</h1>
+    <h3>Francisco</h3>
+    <h4>fcosuport@gmail.com</h4>
+    <h4>(92) 99151-2364</h4>
   </div>
 </template>
